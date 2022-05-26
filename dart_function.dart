79144180033ecebc72e6637b33cee13f7a476c21
Student <64141833+AliNurs@users.noreply.github.com>
2022-05-26 14:32:06 +0600

    Add files via upload

---
+++ dart_function.dart
@@ -0,0 +1,66 @@
+void main() {
+  int a = 5687;
+  double _z = 54.4;
+  String b = '34';
+  b = a.toString();
+  String name = 'Nurs';
+
+  print('Hi there My name is ' + name);
+
+  _z;
+}
+
+
+
+/*void main() {
+  Reader getData = Reader(
+      fullName: "Guljigit uulu Nursultan",
+      books: ['Приключения', 'Словарь', 'Энциклопедия'],
+      numTicket: 12,
+      number: 0776027662,
+      faculty: 'Razrabotchik',
+      dateBorn: '26.05.22');
+  getData.takeBoot();
+
+  // Reader getDate = Reader();
+  //  getDate.takeBoot();
+}
+
+class Reader {
+  String fullName;
+  //List<String> fullName = ['Guljigit uulu', 'Nursultan'];
+  int numTicket;
+  String faculty;
+  String dateBorn;
+  int number;
+  List<String> books;
+//
+  Reader(
+      {required this.fullName,
+      required this.books,
+      required this.numTicket,
+      required this.dateBorn,
+      required this.faculty,
+      required this.number});
+
+  void takeBoot() {
+    print('$fullName взял ${books.length} книги');
+    print('$fullName взял $books');
+  }
+}
+
+// class Reader {
+//   //List<String> fullName = ['Guljigit', 'uulu', 'Nursultan'];
+     //String fullName: "Guljigit uulu Nursultan",
+      //int numTicket: 12,
+      //int number: 0776027662,
+      //String faculty: 'Razrabotchik',
+      //String dateBorn: '26.05.22'
+//   List<String> books = ['Приключения', 'Словарь', 'Энциклопедия'];
+
+//   void takeBoot() {
+//     print('$fullName взял ${books.length} книги');
+//     print('$fullName взял $books');
+//   }
+// }
+*/
